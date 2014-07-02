09923f552904430c71de89bde85b9b2037ff72d7
Whitney Young <wbyoung@fadingred.com>
2014-07-02 12:08:43 -0700

    Better session data support.

--- server/application.js
+++ server/application.js
@@ -60,7 +60,7 @@ api.post('/users', admit.create, function(req, res) {
 
 api.post('/sessions', admit.authenticate, function(req, res) {
   // user accessible via req.auth.user
-  res.json({ status: 'ok' });
+  res.json({ session: req.auth.user });
 });
 
 // all routes defined from here on will require authorization
